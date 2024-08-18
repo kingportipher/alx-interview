8b32f46bbfe2cc48f4488bbadaf7f6731e4c493f
Victor Abrokwah <kingportipher@outlook.com>
2024-08-18 21:27:39 +0000

    Task fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-  """Determines if all boxes can be opened.
+    n = len(boxes)
+    unlocked = {0}  # Start with the first box unlocked
+    keys = boxes[0]  # Start with the keys in the first box
 
-  Args:
-    boxes: A list of lists representing boxes and their keys.
+    while keys:
+        new_key = keys.pop()
+        if new_key not in unlocked and new_key < n:
+            unlocked.add(new_key)
+            keys.extend(boxes[new_key])  # Add keys from the newly unlocked box
 
-  Returns:
-    True if all boxes can be opened, False otherwise.
-  """
-
-  num_boxes = len(boxes)
-  visited = [False] * num_boxes
-  visited[0] = True  # First box is unlocked
-
-  def dfs(box_index):
-    for key in boxes[box_index]:
-      if 0 <= key < num_boxes and not visited[key]:
-        visited[key] = True
-        dfs(key)
-
-  dfs(0)
-  return all(visited)
+    return len(unlocked) == n  # Check if all boxes are unlocked
