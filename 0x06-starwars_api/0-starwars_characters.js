8c54568acf079a1b973dd52b88fab33337e570d2
Victor Abrokwah <kingportipher@outlook.com>
2024-09-21 20:12:36 +0000

    Task done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the Movie ID from the command-line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.log('Please provide a movie ID');
+  process.exit(1);
+}
+
+// SWAPI Films endpoint
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Make the request to get movie data
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Parse the response body
+  const movieData = JSON.parse(body);
+
+  // Get the list of character URLs
+  const characters = movieData.characters;
+
+  // Fetch each character's data and print their name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
+
