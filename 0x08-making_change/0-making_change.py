7efc6af81f38df6e3aa43dc7f6db2f43d18cd6fe
Victor Abrokwah <kingportipher@outlook.com>
2024-10-03 18:51:57 +0000

    Task done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
-    #If total is < 0, no coins needed.
+    # If total is 0 or less, no coins are needed
     if total <= 0:
         return 0
 
-    dp = (total + 1) * (total + 1)
+    # Initialize a list to store the minimum number
+    dp = [total + 1] * (total + 1)
 
+    # Zero coins are needed to make a total of 0
     dp[0] = 0
 
+    # Build up the dp array by checking each amount from 1 to total
     for amount in range(1, total + 1):
         for coin in coins:
             if coin <= amount:
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
+    # If dp[total] is still total + 1
     return dp[total] if dp[total] != total + 1 else -1
