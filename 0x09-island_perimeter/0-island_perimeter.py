8a32843a0d334f1ab443e6169c4d6ad28b4c7710
Victor Abrokwah <kingportipher@outlook.com>
2024-10-10 21:53:29 +0000

    Task 0 fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
+"""
+Island Perimeter
+"""
 
 def island_perimeter(grid):
     """
-    Returns the perimeter of the island described in grid
+    alculates the perimeter of the island
     """
-    rows, cols = len(grid), len(grid[0])
-    perimeter = 0
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Add 4 for potential edges
-
-                # Check if left neighbor is water
-                if col - 1 < 0 or grid[row][col - 1] == 0:
-                    perimeter += 1
-
-                # Check if right neighbor is water
-                if col + 1 >= cols or grid[row][col + 1] == 0:
-                    perimeter += 1
-
-                # Check if top neighbor is water (if not on top row)
-                if row - 1 >= 0 and grid[row - 1][col] == 0:
-                    perimeter += 1
-
-                # Check if bottom neighbor is water (if not on bottom row)
-                if row + 1 < rows and grid[row + 1][col] == 0:
-                    perimeter += 1
-
-    return perimeter
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
