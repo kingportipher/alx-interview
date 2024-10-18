7bc6c7c1c4f3b8d8fd570573913fc36e83af536a
Victor Abrokwah <kingportipher@outlook.com>
2024-10-18 05:01:39 +0000

    Task fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,58 @@
 #!/usr/bin/python3
 
-def sieve_of_eratosthenes(n):
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-    return primes
-
-def count_primes_up_to(primes, n):
-    return sum(1 for i in range(1, n + 1) if primes[i])
-
 def isWinner(x, nums):
-    if not nums or x < 1:
-        return None
+    """Determine the winner of the prime game played by Maria and Ben."""
+    maria_score = 0
+    ben_score = 0
 
-    # Determine the maximum value of `n` to consider
-    max_num = max(nums)
-    
-    # Use Sieve of Eratosthenes to precompute prime numbers up to `max_num`
-    primes = sieve_of_eratosthenes(max_num)
-    
-    # Track the scores of each player
-    maria_wins = 0
-    ben_wins = 0
+    # Play each game round based on the numbers in `nums`
+    for num in nums:
+        # Create a list of all integers from 1 to `num`
+        available_numbers = list(range(1, num + 1))
+        # Identify all prime numbers within the range
+        primes = get_primes(1, num)
 
-    # Play each round of the game
-    for n in nums:
-        # Determine the number of primes up to `n`
-        prime_count = count_primes_up_to(primes, n)
-        
-        # If the prime count is odd, Maria wins; otherwise, Ben wins
-        if prime_count % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
+        # If there are no prime numbers, Ben wins the round
+        if not primes:
+            ben_score += 1
+            continue
 
-    # Determine the overall winner based on who has more wins
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+        # Start the game with Maria's turn
+        maria_turn = True
+        while True:
+            # If no primes are left, determine the winner of the current round
+            if not primes:
+                if maria_turn:
+                    ben_score += 1
+                else:
+                    maria_score += 1
+                break
+
+            # Pick the smallest prime, remove it and its multiples
+            smallest_prime = primes.pop(0)
+            available_numbers.remove(smallest_prime)
+            available_numbers = [n for n in available_numbers if n % smallest_prime != 0]
+
+            # Switch turns
+            maria_turn = not maria_turn
+
+    # Determine the overall winner based on the scores
+    if maria_score > ben_score:
+        return "Winner: Maria"
+    if ben_score > maria_score:
+        return "Winner: Ben"
     return None
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def get_primes(start, end):
+    """Retrieve a list of prime numbers within a specified range."""
+    return [num for num in range(start, end + 1) if is_prime(num)]
 
