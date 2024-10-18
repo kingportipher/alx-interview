8afb09e88d5f281e47b5b3b373d5ca5f799d9641
Victor Abrokwah <kingportipher@outlook.com>
2024-10-18 04:54:50 +0000

    Task done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 
-def is_prime(num):
-  """Checks if a number is prime."""
-  if num <= 1:
-    return False
-  for i in range(2, int(num**0.5) + 1):
-    if num % i == 0:
-      return False
-  return  
- True
+def is_winner(rounds, numbers):
+    if rounds <= 0 or numbers is None:
+        return None
+    if rounds != len(numbers):
+        return None
 
-def find_primes(n):
-  """Finds all prime numbers up to n."""
-  primes = []
-  for num in range(2, n + 1):
-    if is_prime(num):
-      primes.append(num)
-  return primes
+    ben_score = 0
+    maria_score = 0
+    primes = [1] * (max(numbers) + 1)
+    primes[0], primes[1] = 0, 0
 
-def isWinner(x, nums):
-  """Determines the winner of the game."""
-  maria_wins = 0
-  ben_wins = 0
-  for n in nums:
-    primes = find_primes(n)
-    while primes:
-      primes = [prime for prime in primes if not any(multiple % prime == 0 for multiple in primes)]
-    if len(primes) % 2 == 0:
-      ben_wins += 1
-    else:
-      maria_wins += 1
-  if maria_wins > ben_wins:
-    return "Maria"
-  elif ben_wins > maria_wins:
-    return "Ben"
-  else:
+    for i in range(2, len(primes)):
+        mark_non_primes(primes, i)
+
+    for n in numbers:
+        if sum(primes[:n + 1]) % 2 == 0:
+            ben_score += 1
+        else:
+            maria_score += 1
+
+    if ben_score > maria_score:
+        return "Ben"
+    if maria_score > ben_score:
+        return "Maria"
     return None
+
+def mark_non_primes(array, number):
+    for i in range(2, len(array)):
+        try:
+            array[i * number] = 0
+        except (ValueError, IndexError):
+            break
+
