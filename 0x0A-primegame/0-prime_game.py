875733e3ee6600d4af00d841a645fa018cf6b866
Victor Abrokwah <kingportipher@outlook.com>
2024-10-18 04:38:40 +0000

    Task fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,53 +1,50 @@
 #!/usr/bin/python3
 
-"""
-Prime Game
-"""
 def find_multiples(num, targets):
-    """
-    Remove multiples of the given number from the list.
-    """
-    or i in targets[:]:
+    for i in targets[:]:
         if i % num == 0:
             targets.remove(i)
     return targets
+
 def is_prime(i):
-    if i <= 1:
+    if i == 1:
         return False
-    for j in range(2, int(i ** 0.5) + 1):
+    for j in range(2, i):
         if i % j == 0:
             return False
     return True
+
 def find_primes(n):
-    prime_count = 0
-    targets = list(n)
-
-    for i in range(2, max(targets) + 1):
+    counter = 0
+    target = list(n)
+    for i in range(1, len(target) + 1):
         if is_prime(i):
-            prime_count += 1
-            if i in targets:
-                targets.remove(i)
-            targets = find_multiples(i, targets)
+            counter += 1
+            target.remove(i)
+            target = find_multiples(i, target)
+    return counter
 
-    return prime_count
-
-def isWinner(x, nums):
+def is_winner(x, nums):
     players = {'Maria': 0, 'Ben': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        num = nums[elem]
+        for i in range(1, num + 1):
+            cluster.add(i)
+            if i == num + 1:
+                break
+        temp = find_primes(cluster)
 
-    for game_round in range(x):
-        n = nums[game_round]
-        numbers = set(range(1, n + 1))
-        prime_count = find_primes(numbers)
-
-        # Determine who wins this round based on the number of primes found
-        if prime_count % 2 == 0:
+        if temp % 2 == 0:
             players['Ben'] += 1
-        else:
+        elif temp % 2 != 0:
             players['Maria'] += 1
 
     if players['Maria'] > players['Ben']:
         return 'Maria'
-    elif players['Ben'] > players['Maria']:
+    elif players['Maria'] < players['Ben']:
         return 'Ben'
     else:
         return None
+
