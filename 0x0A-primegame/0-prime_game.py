f685e08f332644f681650ca7fde0d98ae50adb7b
Victor Abrokwah <kingportipher@outlook.com>
2024-12-12 09:51:27 -0800

    Task done

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    if x <= 0 or not nums:
+        return None
+    def sieve_of_eratosthenes(n):
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, n + 1, i):
+                    is_prime[j] = False
+        return is_prime
+    max_n = max(nums)
+    prime_flags = sieve_of_eratosthenes(max_n)
+
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if prime_flags[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    return None
