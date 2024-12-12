73b8123fdd5f84a46922319bf6b48748106b1c7d
Victor Abrokwah <kingportipher@outlook.com>
2024-12-12 09:55:44 -0800

    Task fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+"""
+isWinner function to solve the Prime Game problem.
+"""
+
+def primes(n):
+    """Generate a list of prime numbers up to 'n'.
+    """
+    prime_numbers = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            prime_numbers.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime_numbers
 
 def isWinner(x, nums):
-    if x <= 0 or not nums:
+    """Determine the winner of the prime_numbers Game.
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    def sieve_of_eratosthenes(n):
-        is_prime = [True] * (n + 1)
-        is_prime[0] = is_prime[1] = False
-        for i in range(2, int(n**0.5) + 1):
-            if is_prime[i]:
-                for j in range(i * i, n + 1, i):
-                    is_prime[j] = False
-        return is_prime
-    max_n = max(nums)
-    prime_flags = sieve_of_eratosthenes(max_n)
 
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if prime_flags[i] else 0)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_count[n] % 2 == 0:
+    maria_wins = ben_wins = 0
+    for i in range(x):
+        prime_numbers = primes(nums[i])
+        if len(prime_numbers) % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
